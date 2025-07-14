cee6dac5f0e219b0a9f81c8b9b044dc29689cd57
Yerson Palacio <ympalacio@celerix.com>
2025-07-14 14:01:07 -0500

    Nuevas funcionalidades de borrar y consultar customers

--- Proyecto/main.py
+++ Proyecto/main.py
@@ -1,5 +1,5 @@
 import zoneinfo
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from datetime import datetime
 from models import Customer, Transaction, Invoice, CustomerCreate
 from bd import SessionDep, create_all_tables
@@ -36,6 +36,24 @@ async def create_customer(customer_data: CustomerCreate, session: SessionDep):
     session.commit()
     session.refresh(customer)
     return customer
+
+
+@app.get("/customers/{customer_id}", response_model=Customer)
+async def read_customer(customer_id: int, session: SessionDep):
+    customer_bd = session.get(Customer, customer_id)
+    if not customer_bd:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="El cliente no existe")
+    return customer_bd
+
+
+@app.delete("/customers/{customer_id}")
+async def delete_customer(customer_id: int, session: SessionDep):
+    customer_bd = session.get(Customer, customer_id)
+    if not customer_bd:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="El cliente no existe")
+    session.delete(customer_bd)
+    session.commit()
+    return {"detail" : "Ok"}
 
 
 @app.get("/customers", response_model=list[Customer])
